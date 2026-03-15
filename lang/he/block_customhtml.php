d05ab33590fda0dd6e0ec95d878218a47f16bf41
Mattan Dor <mattan.dor1@gmail.com>
2026-03-15 15:25:15 +0200

    Enhance cohort visibility settings in Custom HTML block
    - Improved user access control by refining cohort selection functionality in edit_form.php.
    - Updated language strings for better clarity in cohort settings within the block_customhtml language file.

---
+++ lang/he/block_customhtml.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_customhtml', language 'he', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_customhtml
+ * @copyright  CentricApp LTD dev@centricapp.co.il
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['allowadditionalcssclasses'] = 'אפשר מחלקות CSS נוספות';
+$string['configallowadditionalcssclasses'] = 'מוסיף אפשרות בהגדרות משבצת HTML מותאמת להגדרת מחלקות CSS נוספות.';
+$string['configclasses'] = 'מחלקות CSS נוספות';
+$string['configclasses_help'] = 'מטרת ההגדרה היא לסייע בעיצוב ולהבדיל בין משבצות טקסט. מחלקות CSS שהוזנו כאן (מופרדות ברווח) יתווספו למחלקות ברירת המחדל של המשבצת.';
+$string['configcontent'] = 'תוכן';
+$string['configcohort'] = 'הצג רק לקבוצת מערכת (cohort)';
+$string['configcohort_help'] = 'אם נבחרה קבוצת מערכת, רק משתמשים השייכים לקבוצה זו יראו משבצת זו ואת תוכנה. אם נבחר "הצג לכולם", המשבצת תוצג לכל המשתמשים.';
+$string['configcohortnone'] = 'הצג לכולם';
+$string['configtitle'] = 'כותרת משבצת HTML מותאמת';
+$string['customhtml:addinstance'] = 'הוסף משבצת HTML מותאמת חדשה';
+$string['customhtml:myaddinstance'] = 'הוסף משבצת HTML מותאמת ללוח הבקרה';
+$string['newhtmlblock'] = '(משבצת HTML מותאמת חדשה)';
+$string['pluginname'] = 'HTML מותאם';
+$string['search:content'] = 'תוכן משבצת HTML מותאמת';
+$string['privacy:metadata:block'] = 'משבצת ה-HTML המותאמת שומרת את כל הנתונים שלה במערכת המשבצות.';
